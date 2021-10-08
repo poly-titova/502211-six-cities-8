623f6a10933377fb02693444673b9d14e267b8aa
Poly Titova <poly_titova@mail.ru>
2021-10-08 21:05:26 +0300

    Создали компонент для описания приватных маршрутов

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import MainScreen from '../main-screen/main-screen';
 import SignInScreen from '../signin-screen/signin-screen';
 import FavoritesScreen from '../favorites-screen/favorites-screen';
 import RoomScreen from '../room-screen/room-screen';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   offersCount: number;
@@ -20,9 +21,13 @@ function App({ offersCount }: AppScreenProps): JSX.Element {
         <Route exact path={AppRoute.Login}>
           <SignInScreen />
         </Route>
-        <Route exact path={AppRoute.Favorites}>
-          <FavoritesScreen />
-        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <FavoritesScreen />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
         <Route exact path={AppRoute.Room}>
           <RoomScreen />
         </Route>
